2f23fedefb192e339528703b95873edfff5df7ad
vivianm <vivianmbaji@gmail.com>
2013-12-10 13:43:22 +0300

    Using <<NAME that works like a string and you can put as many lines of text until you type NAME

--- ex10.rb
+++ ex10.rb
@@ -1,2 +1,18 @@
 puts "I am 6'2\" tall." # escaping double-quotes inside string
 puts 'I am 6\'2" tall.' # escaping single-quotes inside string
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = <<MY_HEREDOC
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+MY_HEREDOC
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
