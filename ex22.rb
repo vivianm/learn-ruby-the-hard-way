066faea0a981a13d38ec97ec6a28368eb4dd46f9
vivianm <vivianmbaji@gmail.com>
2013-12-11 16:32:53 +0300

    Adding What I know

---
+++ ex22.rb
@@ -0,0 +1,20 @@
+# This is a comment, anything after this symbol is ignored
+# Its is also used to disable or comment out about a piece of codes.
+puts " " # prints out results and generates a new line
+print " " # prints results but doesn't generate new line
+"#{__}"  # gives out a declared variable
+_ (underscore) shows imaginary space
+%name # shows references by name
+%[name, name1] # shows multiple references by names
+\" # escaping double quotes
+'\ # escaping single quotes
+\n # generates a new line
+gets.chomp() # allows user to input
+ARGV # contains the arguments passed to script, one per element
+prompt # allows to enter(prompt) for user input
+File.open() # opens a file
+STDIN.gets.chomp() # it gets input and returns string
+input.read() # method to read the content of opened file
+def # defines a function
+end # ends a function
+*args # specifies a variable length argument list 
