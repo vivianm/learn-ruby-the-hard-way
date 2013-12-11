07e38904fa99e5a81eb7a7dd0efc5b7dc7e2e0ce
vivianm <vivianmbaji@gmail.com>
2013-12-11 18:13:14 +0300

    Correcting a command print

--- ex26.rb
+++ ex26.rb
@@ -81,7 +81,7 @@ puts "We can also do that this way:"
 puts "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_pont
 
 
-sentence = "All god things come to those who wait."
+sentence = "All good\tthings come to those who wait."
 
 words = ex25.break_words(sentence)
 sorted_words = ex25.sort_words(words)
@@ -91,7 +91,7 @@ puts_last_word(words)
 .puts_first_word(sorted_words)
 puts_last_word(sorted_words)
 sorted_words = ex25.sort_sentence(sentence)
-prin sorted_words
+print_sorted_words
 
 puts_first_and_last(sentence)
 
