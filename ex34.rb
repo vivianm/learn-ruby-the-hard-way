fa8355e8958eadcaf98361e3b6e3a6e8c9e954ec
vivianm <vivianmbaji@gmail.com>
2013-12-12 11:20:04 +0300

    The Animal at 3

--- ex34.rb
+++ ex34.rb
@@ -2,3 +2,4 @@ animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
 puts "The animal at 1: #{animals[1]}"
 puts "The 3rd animal: #{animals[2]}"
 puts "The 1st animal: #{animals[0]}"
+puts "The animal at 3: #{animals[3]}"
