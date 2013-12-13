8e5c6fc0b65f8cda4961f8b374febbfe7782e947
vivianm <vivianmbaji@gmail.com>
2013-12-13 11:31:40 +0300

    Accessing the variable using the :: (double-colon) operator instead of the . (dot)

--- get_thing.rb
+++ get_thing.rb
@@ -7,9 +7,13 @@ module Mystuff
 	def Mystuff.apple()
 	    puts "I AM APPLES!" 
 	end
+
+	#this is just a variable
+	TANGERINE = "Living reflection of a dream"
 end	  
 
 # use that module with require and then access the apple function
 require 'mystuff'
 
 Mystuff.apple()
+puts Mystuff::TANGERINE
