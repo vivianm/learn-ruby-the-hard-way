2901789769a5e8254f861f65555c0a9eff8d678e
vivianm <vivianmbaji@gmail.com>
2013-12-13 11:39:59 +0300

    Adding A Class for Apple

--- get_thing.rb
+++ get_thing.rb
@@ -22,3 +22,18 @@ puts Mystuff::TANGERINE
 mystuff['apple'] # get apple from hash
 MyStuff.apple() # get apple from module
 MyStuff::TANGERINE # same thing, it's just a variable
+
+
+# Adding A class for the same thing above(Classes are like Module)
+class MyStuff
+
+	def initialize()
+		@tangerine = "And now a thousand years between"
+	end
+	
+	attr_reader :tangerine
+
+	def apple()
+	    puts "I AM CLASSY APPLES!"
+	end
+end	    	
