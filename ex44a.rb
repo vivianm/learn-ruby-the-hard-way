f4cfae1a51a547deb5cd58dabda47d93e6527d7b
vivianm <vivianmbaji@gmail.com>
2013-12-14 12:14:22 +0300

    Adding All Three Combined

--- ex44a.rb
+++ ex44a.rb
@@ -43,4 +43,45 @@ dad = Parent.new()
 son = Child.new()
 
 dad.altered()
-son.altered()         	     
+son.altered()         	   
+
+# final version that shows each kind of interaction from inheritance in one file.
+class Parent
+
+    def override()
+        puts "PARENT override()"
+    end
+
+    def implicit()
+        puts "PARENT implicit()"
+    end
+
+    def altered()
+        puts "PARENT altered()"
+    end
+end
+
+class Child < Parent
+    
+    def override()
+        puts "CHILD override()"
+    end
+
+    def altered()
+        puts "CHILD, BEFORE PARENT altered()"
+        super()
+        puts "CHILD, AFTER PARENT altered()"
+    end
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()  
