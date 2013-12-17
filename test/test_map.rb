442fde66c56656c86fcb8ff11ae21af9eb9d371b
vivianm <vivianmbaji@gmail.com>
2013-12-17 13:34:34 +0300

    New Automated test

--- test/test_map.rb
+++ test/test_map.rb
@@ -1,12 +1,12 @@
 require 'test/unit'
-require_relative '../lib/ex47'
+require_relative '../lib/map'
 
 class MyUnitTests < Test::Unit::TestCase
 
   def test_room()
     gold = Room.new("GoldRoom", 
-                    """This room has gold in it you can grab. There's a
-                door to the north.""")
+                    %q{This room has gold in it you can grab. There's a
+                door to the north.})
     assert_equal(gold.name, "GoldRoom")
     assert_equal(gold.paths, {})
   end
@@ -16,9 +16,9 @@ class MyUnitTests < Test::Unit::TestCase
     north = Room.new("North", "Test room in the north.")
     south = Room.new("South", "Test room in the south.")
 
-    center.add_paths({:north => north, :south => south})
-    assert_equal(center.go(:north), north)
-    assert_equal(center.go(:south), south)
+    center.add_paths({'north' => north, 'south' => south})
+    assert_equal(center.go('north'), north)
+    assert_equal(center.go('south'), south)
   end
 
   def test_map()
@@ -26,13 +26,20 @@ class MyUnitTests < Test::Unit::TestCase
     west = Room.new("Trees", "There are trees here, you can go east.")
     down = Room.new("Dungeon", "It's dark down here, you can go up.")
 
-    start.add_paths({:west => west, :down => down})
-    west.add_paths({:east => start})
-    down.add_paths({:up => start})
+    start.add_paths({'west' => west, 'down' => down})
+    west.add_paths({'east' => start})
+    down.add_paths({'up' => start})
 
-    assert_equal(start.go(:west), west)
-    assert_equal(start.go(:west).go(:east), start)
-    assert_equal(start.go(:down).go(:up), start)
+    assert_equal(start.go('west'), west)
+    assert_equal(start.go('west').go('east'), start)
+    assert_equal(start.go('down').go('up'), start)
   end
 
+  def test_gothonweb_game_map()
+    assert_equal(START.go('shoot!'), generic_death)
+    assert_equal(START.go('dodge'), generic_death)
+
+    room = START.go('tell a joke')
+    assert_equal(room, laser_weapon_armory)
+  end
 end
